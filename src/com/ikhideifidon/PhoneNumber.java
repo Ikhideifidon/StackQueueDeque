7ec237a5b1424993a874f6f7f7042cc52a724d88
ikhideifidon <6172309922LOVEe$%&>
2022-02-05 10:55:46 -0500

    commit

---
+++ src/com/ikhideifidon/PhoneNumber.java
@@ -0,0 +1,63 @@
+package com.ikhideifidon;
+
+public final class PhoneNumber {
+    private final short areaCode;
+    private final short prefix;
+    private final short lineNum;
+
+    public PhoneNumber(short areaCode, short prefix, short lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "area code");
+        this.prefix = rangeCheck(prefix, 999, "prefix");
+        this.lineNum = rangeCheck(lineNum, 9999, "line num");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max)
+            throw new IllegalArgumentException(arg + ": " + val);
+        return (short) val;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+        if (!(o instanceof PhoneNumber other))                  // Use of pattern variable
+            return false;
+        return areaCode == other.areaCode && prefix == other.prefix && lineNum == other.lineNum;
+    }
+
+    /**
+     * hashCode method with lazily initialized cached hash code
+    private int hashCode;                   // Automatically initialized to 0
+
+    public int hashCode() {
+        int result = hashCode;
+        if (result == 0) {
+            result = Short.hashCode(areaCode);
+            result = 31 * result + Short.hashCode(prefix);
+            result = 31 * result + Short.hashCode(lineNum);
+            hashCode = result;
+        }
+        return result;
+    }
+     */
+
+    @Override
+    public int hashCode() {
+        int result = Short.hashCode(areaCode);
+        result = 31 * result + Short.hashCode(prefix);
+        result = 31 * result + Short.hashCode(lineNum);
+        return result;
+    }
+
+    // Clone method for class with no references to mutable state
+    @Override
+    public PhoneNumber clone() {
+        try {
+            return (PhoneNumber) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+}
