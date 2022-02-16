b284f86b094f231e5ef1b8f9d6d77f5562ca24d2
ikhideifidon <6172309922LOVEe$%&>
2022-02-15 19:52:35 -0500

    Custom EmptyLinkedListException Implementation

---
+++ src/com/ikhideifidon/EmptyLinkedListException.java
@@ -0,0 +1,21 @@
+package com.ikhideifidon;
+
+
+public class EmptyLinkedListException extends Exception {
+    public EmptyLinkedListException(String message) {
+        super(message);
+    }
+    public EmptyLinkedListException(Throwable cause) {
+        super(cause);
+    }
+    public EmptyLinkedListException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public EmptyLinkedListException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public EmptyLinkedListException() {
+
+    }
+}
